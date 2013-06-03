6a47fe8cb293fc69c21f3f1d8e4e9b4b04e782a1
Aria Stewart <aredridel@nbtsc.org>
2013-06-02 23:05:24 -0400

    Better deps, more tests

--- test/basics.js
+++ test/basics.js
@@ -7,6 +7,23 @@ test('A string with no expansions returns the string unmodified', function(t) {
 });
 
 test('A simple pre-phase expansion', function(t) {
-    t.plan(1);
+    t.plan(7);
     t.equal(expansions.expandString('<(a)', { a: '1' }, 'pre'), '1');
+    t.equal(expansions.expandString('<(a)23', { a: '1' }, 'pre'), '123');
+    t.equal(expansions.expandString('0<(a)2', { a: '1' }, 'pre'), '012');
+    t.equal(expansions.expandString('<(a))', { a: '1' }, 'pre'), '1)');
+    t.equal(expansions.expandString('(<(a)', { a: '1' }, 'pre'), '(1');
+    t.equal(expansions.expandString('<<(a)', { a: '1' }, 'pre'), '<1');
+    t.equal(expansions.expandString('>(a)', { a: '1' }, 'pre'), '>(a)');
+});
+
+test('A simple post-phase expansion', function(t) {
+    t.plan(7);
+    t.equal(expansions.expandString('>(a)', { a: '1' }, 'post'), '1');
+    t.equal(expansions.expandString('>(a)23', { a: '1' }, 'post'), '123');
+    t.equal(expansions.expandString('0>(a)2', { a: '1' }, 'post'), '012');
+    t.equal(expansions.expandString('>(a))', { a: '1' }, 'post'), '1)');
+    t.equal(expansions.expandString('(>(a)', { a: '1' }, 'post'), '(1');
+    t.equal(expansions.expandString('>>(a)', { a: '1' }, 'post'), '>1');
+    t.equal(expansions.expandString('<(a)', { a: '1' }, 'post'), '<(a)');
 });
