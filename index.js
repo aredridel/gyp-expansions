45dadc68fe9028f6386d87e130c8c988a459d98d
Aria Stewart <aredridel@nbtsc.org>
2013-06-06 22:19:46 -0400

    Parse into an intermediate tree

--- index.js
+++ index.js
@@ -10,13 +10,37 @@ var BSJSTranslator = ometajs_.grammars.BSJSTranslator;
 
 var cp = require("child_process");
 
-function h() {
-    console.log("!");
-    return true;
-}
-
 function outputOf(command) {
     return command;
+}
+
+function command(c) {
+    return {
+        type: "command",
+        command: c
+    };
+}
+
+function str(s) {
+    return {
+        type: "string",
+        value: s
+    };
+}
+
+function ref(c, v) {
+    return {
+        type: "ref",
+        variable: c,
+        value: v[c]
+    };
+}
+
+function args(a) {
+    return {
+        type: "arguments",
+        value: a
+    };
 }
 
 var Expansion = function Expansion(source, opts) {
@@ -56,7 +80,7 @@ Expansion.prototype["ref"] = function $ref() {
                     return this._match(")");
                 }, true) && this._rule("char", false, [], null, this["char"]);
             });
-        }) && (c = this._getIntermediate(), true) && this._match(")") && this._exec(this._options.variables[c.join("")]);
+        }) && (c = this._getIntermediate(), true) && this._match(")") && this._exec(ref(c.join(""), this._options.variables));
     }) || this._atomic(function() {
         var c;
         return (this._atomic(function() {
@@ -66,29 +90,23 @@ Expansion.prototype["ref"] = function $ref() {
         })) && this._match("!") && (this._match("@") || this._atomic(function() {
             return this._rule("empty", false, [], null, this["empty"]);
         })) && this._match("(") && this._rule("command", false, [], null, this["command"]) && (c = this._getIntermediate(), 
-        true) && this._match(")") && this._exec(outputOf(c));
+        true) && this._match(")") && this._exec(command(c));
     });
 };
 
 Expansion.prototype["command"] = function $command() {
     return this._atomic(function() {
         var a;
-        return this._match("[") && this._rule("arguments", false, [], null, this["arguments"]) && (a = this._getIntermediate(), 
-        true) && this._match("]") && this._exec(a);
+        return this._match("[") && this._rule("listOf", false, [ "string", "," ], null, this["listOf"]) && (a = this._getIntermediate(), 
+        true) && this._match("]") && this._exec(args(a));
     }) || this._atomic(function() {
         var s;
         return this._any(function() {
             return this._atomic(function() {
                 return this._rule("stringInside", false, [ ")" ], null, this["stringInside"]);
             });
-        }) && (s = this._getIntermediate(), true) && this._exec(s.join(""));
+        }) && (s = this._getIntermediate(), true) && this._exec(s);
     });
-};
-
-Expansion.prototype["arguments"] = function $arguments() {
-    var s;
-    return this._rule("listOf", false, [ "string", "," ], null, this["listOf"]) && (s = this._getIntermediate(), 
-    true) && this._exec(s);
 };
 
 Expansion.prototype["string"] = function $string() {
@@ -98,14 +116,14 @@ Expansion.prototype["string"] = function $string() {
             return this._atomic(function() {
                 return this._rule("stringInside", false, [ '"' ], null, this["stringInside"]);
             });
-        }) && (c = this._getIntermediate(), true) && this._match('"') && this._exec(c.join(""));
+        }) && (c = this._getIntermediate(), true) && this._match('"') && this._exec(c);
     }) || this._atomic(function() {
         var c;
         return this._match("'") && this._any(function() {
             return this._atomic(function() {
                 return this._rule("stringInside", false, [ "'" ], null, this["stringInside"]);
             });
-        }) && (c = this._getIntermediate(), true) && this._match("'") && this._exec(c.join(""));
+        }) && (c = this._getIntermediate(), true) && this._match("'") && this._exec(c);
     });
 };
 
@@ -116,6 +134,49 @@ Expansion.prototype["stringInside"] = function $stringInside() {
         return this._rule("ref", false, [], null, this["ref"]) && (r = this._getIntermediate(), 
         true) && this._exec(r);
     }) || this._atomic(function() {
+        var p;
+        return this._many(function() {
+            return this._atomic(function() {
+                return !this._atomic(function() {
+                    return this._rule("ref", false, [], null, this["ref"]);
+                }, true) && this._rule("stringInsideParts", false, [ x ], null, this["stringInsideParts"]);
+            });
+        }) && (p = this._getIntermediate(), true) && this._exec(str(p.join("")));
+    }));
+};
+
+Expansion.prototype["stringInsideRaw"] = function $stringInsideRaw() {
+    return this._atomic(function() {
+        var r;
+        return this._rule("ref", false, [], null, this["ref"]) && (r = this._getIntermediate(), 
+        true) && this._exec(r);
+    }) || this._atomic(function() {
+        var p;
+        return this._many(function() {
+            return this._atomic(function() {
+                return !this._atomic(function() {
+                    return this._rule("ref", false, [], null, this["ref"]);
+                }, true) && this._rule("stringInsidePartsRaw", false, [], null, this["stringInsidePartsRaw"]);
+            });
+        }) && (p = this._getIntermediate(), true) && this._exec(str(p.join("")));
+    });
+};
+
+Expansion.prototype["stringInsidePartsRaw"] = function $stringInsidePartsRaw() {
+    return this._atomic(function() {
+        var y;
+        return this._match("\\") && this._rule("char", false, [], null, this["char"]) && (y = this._getIntermediate(), 
+        true) && this._exec(y);
+    }) || this._atomic(function() {
+        var c;
+        return this._rule("char", false, [], null, this["char"]) && (c = this._getIntermediate(), 
+        true) && this._exec(c);
+    });
+};
+
+Expansion.prototype["stringInsideParts"] = function $stringInsideParts() {
+    var x;
+    return this._skip() && (x = this._getIntermediate(), true) && (this._atomic(function() {
         var y;
         return this._match("\\") && this._rule("char", false, [], null, this["char"]) && (y = this._getIntermediate(), 
         true) && this._exec(y);
@@ -123,7 +184,7 @@ Expansion.prototype["stringInside"] = function $stringInside() {
         var c;
         return this._atomic(function() {
             return !this._atomic(function() {
-                return this._rule("token", true, [ x ], null, this["token"]);
+                return this._rule("seq", false, [ x ], null, this["seq"]);
             }, true) && this._rule("char", false, [], null, this["char"]);
         }) && (c = this._getIntermediate(), true) && this._exec(c);
     }));
@@ -133,20 +194,21 @@ Expansion.prototype["stringRaw"] = function $stringRaw() {
     var c;
     return this._any(function() {
         return this._atomic(function() {
-            return this._rule("stringInside", false, [ "" ], null, this["stringInside"]);
+            return this._rule("stringInsideRaw", false, [], null, this["stringInsideRaw"]);
         });
-    }) && (c = this._getIntermediate(), true) && this._exec(c.join(""));
+    }) && (c = this._getIntermediate(), true) && this._exec(c);
 };
 
 var expansions = module.exports = {
     parser: Expansion,
     expandString: function(s, variables, which, cb) {
-        cb(null, Expansion.matchAll(s, "stringRaw", {
+        var tree = Expansion.matchAll(s, "stringRaw", {
             variables: variables,
             phase: which
-        }));
+        });
+        cb(null, tree);
     },
-    expandArray: function(a, variables, which) {
+    expandArray: function(a, variables, which, cb) {
         var re = which == "pre" ? /^<@\((.*)\)$/ : /^>@\((.*)\)$/;
         var out = [];
         var m;
@@ -163,6 +225,6 @@ var expansions = module.exports = {
                 out.push(a[i]);
             }
         }
-        return out;
+        cb(null, out);
     }
 };
